6fb547dc4e8d5f0a60190d12b1d5ba544cefe5aa
jwkim <jwkim98@kaist.ac.kr>
2018-09-18 22:08:19 +0900

    some more functions added.. but not completed

--- mul_gpu.cu
+++ mul_gpu.cu
@@ -35,20 +35,22 @@ class cudaMatrix{
         }
 
         __global__
-        static void matAdd(int N, float *A, float *B, float *C){
+        static void matAdd(int M, int N, float *A, float *B, float *C){
         
             int index = threadIdx.x;
             int stride = blockDim.x;
-            for(int i=index; i < N ; i += stride)
+            int totalelements = M*N;
+            for(int i = index; i < M*N; i += stride)
                 C[i] = A[i] + B[i];
         }
 
        __global__
-       static void matDot(int N, float* A, float* B, float* C){
+       static void matDot(int M, int N, float* A, float* B, float* C){
        
             int index = threadIdx.x;
             int stride = blockDim.x;
-            for(int i=index; i < n ; i += stride)
+            int totalelements = M*N;
+            for(int i=index; i < totalelements; i += stride)
                 C[i] = A[i] * B[i];
        }
 
@@ -61,8 +63,15 @@ class cudaMatrix{
                B[col*M + row] = A[row*N + col];
        }
 
+
+       __global__
+           static void matOperation(int M, int N, float*A, flout *B){
+           }
+
+       
+
   public:
-       static void MatMul(float N, float *inA, float *inB, float *outC){
+       static void MatMul(int N, float *inA, float *inB, float *outC){
 
            float *g_inA, *g_inB, *g_outC;
 
@@ -74,12 +83,58 @@ class cudaMatrix{
            cudaMemcpy(g_inB, inB, N*N*sizeof(float));
            //cudaMemcpy(g_outC, outC, N*N*sizeof(float));
 
-           matmul<<<1,256>>>(inA, inB, outC, N);
+           matMul<<<1,256>>>(inA, inB, outC, N);
 
            cudaMemcpy(outC, g_outC, N*N*sizeof(float), cudaMemcpyDeviceToHost);
 
        }
 
+       static void MatMul(int M, int MN, int N, float *inA, float *inB, float *outC){
+           
+           float *g_inA, *g_inB, *g_outC;
 
+           cudaMalloc((void**)&g_inA, N*MN*sizeof(float), cudaMemcpyHostToDevice);
+           cudaMalloc((void**)&g_inB, MN*N*sizeof(float), cudaMemcpyHostToDevice);
+           cudaMalloc((void**)&g_outC, M*N*sizeof(float), cudaMemcpyHostToDevice);
+           
+           cudaMemcpy(g_inA, inA, N*MN*sizeof(float));
+           cudaMemcpy(g_inB, inB, MN*N*sizeof(float));
+
+           matMul<<<1,256>>>(inA, inB, outC, M, MN, N);
+
+           cudaMemcpy(outC, g_outC, M*N*sizeof(float), cudaMemcpyDeviceToHost);
+       }
+
+       static void MatAdd(int M, int N, float *inA, float *inB, float *outC){
+
+           float *g_inA, *g_inB, *g_outC;
+
+           cudaMalloc((void**)&g_inA, M*N*sizeof(float), cudaMemcpyHostToDevice);
+           cudaMalloc((void**)&g_inB, M*N*sizeof(float), cudaMemcpyHostToDevice);
+           cudaMalloc((void**)&g_outC, M*N*sizeof(float), cudaMemcpyHostToDevice);
+           
+           cudaMemcpy(g_inA, inA, M*N*sizeof(float));
+           cudaMemcpy(g_inB, inB, M*N*sizeof(float));
+
+           matAdd<<<1,256>>>(M, N ,inA, inB, outC);
+
+           cudaMemcpy(outC, g_outC, M*N*sizeof(float), cudaMemcpyDeviceToHost);
+       }
+
+       static void MatDot(int M, int N, float *inA, float *inB, float *outC){
+
+           float *g_inA, *g_inB, *g_outC;
+
+           cudaMalloc((void**)&g_inA, M*N*sizeof(float), cudaMemcpyHostToDevice);
+           cudaMalloc((void**)&g_inB, M*N*sizeof(float), cudaMemcpyHostToDevice);
+           cudaMalloc((void**)&g_outC, M*N*sizeof(float), cudaMemcpyHostToDevice);
+           
+           cudaMemcpy(g_inA, inA, M*N*sizeof(float));
+           cudaMemcpy(g_inB, inB, M*N*sizeof(float));
+
+           matDot<<<1,256>>>(M, N ,inA, inB, outC);
+
+           cudaMemcpy(outC, g_outC, M*N*sizeof(float), cudaMemcpyDeviceToHost);
+       }
 
 }
