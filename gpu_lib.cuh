9e23ba33b057ad3a286fbff5b0e5bc06a1aa153e
jwkim98 <jwkim98@kaist.ac.kr>
2018-09-25 17:55:49 +0900

    turned mul_gpu into header file

---
+++ gpu_lib.cuh
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <math>
+#include <stdlib.h>
+#define PAGE_SIZE = 1024*4 // for x86 systems, page size is 4KB 
+
+class cudaMatrix{
+
+  private:
+        //for 2-dimensional Squre Matrix
+      template<typename data_type> //declares template data_type
+        __global__
+        static void matMul(float* A, float* B, float* C, int N){
+
+            int row = blockIdx.y*blockDim.y + threadIdx.y;
+            int col = blockIdx.x*blockDim.x + threadIdx.x;
+            if (row < N && col < N){
+                float sum = 0.0f;
+                for(int i=0; i < N; i++){
+                    sum += A[row*N + i] * B[col + i*N];
+                }
+                C[row*N + col] = sum;
+            }
+        }
+
+      template<typename data_type>
+        __global__
+        static void matMulUnified(int N ,data_type* A ,data_type* B, data_type* C){
+            size_t tid = threadIdx.x + blockIdx.x * blockDim.x;
+            size_t tidRow = tid/N;
+            size_t tidCol = tid%N; 
+            size_t n = N*N;
+            size_t data_size = sizeof(data_type);
+
+            if(tidRow < rows && tidCol < rows){
+                data_type accum = 0;
+                for(tid; tid < n; tid += blockDim.x * gridDim.x){
+                    for(int i = 0; i < rows; i++){
+                        int indexRow = tid*tidRow + i;
+                        int indexCol = i*rows + tidCol;
+                        accum += A[indexRow] * B[indexCol];
+                    }
+                    data_type*C[tidRow*row + tidCol] = accum;
+                }
+            }
+        }
+
+      template <typename data_type>
+            __global__
+        static void matMulTwo(int M ,int MN ,int N ,data_type *input_A ,data_type* input_B ,data_type* output_C){
+            int row = blockIdx.y*blockDim.y + threadIdx.y; //row for this thread to calculate
+            int col = blockIdx.x*blockDim.x + threadIdx.x; //column for this thread to calculate
+           
+
+            //Result would be M*N matrix
+            if(row < M && col < N){
+                data_type sum = 0;
+                for(int i=0; i < MN; i++){
+                    sum += input_A[row*MN + i] * input_B[col + i*N];
+                }
+                C[row*N + col] = sum;
+            }
+            //TODO think about matrices exceeding the size of whold grid..
+        }
+
+        //for Any 2-dimensional matrices
+      template <typename data_type>
+        __global__
+        static void matOp(int M ,int N ,data_type *input ,data_type* output ,std::function<data_type (data_type)> accumulate){
+            //current row and column for this thread to execute
+            int row = blockIdx.y*blockDim.y + threadIdx.y; //row for this thread to calculate
+            int col = blockIdx.x*blockDim.x + threadIdx.x; //column for this thread to calculate
+
+            int linearId= blockDim.x * gridDim.x * row +col;
+
+            //I made whole 2-dimensional array linear, and mapped it to warp ID by dividing it into 32
+            int warp_id = linearId>>5; //devide by 32
+            int warps_per_grid = (blockDim.x*gridDim.x*blockDim.y*gridDim*y) >>5; //whole dimension of each grid devided by 32(warp size)
+
+            size_t warps_Required = (M*MN * sizeof(data_type) + PAGE_SIZE -1) / PAGE_SIZE;
+
+            int lane_id = linearId%32;
+
+            for(; warp_id < warps_Required ; warp_id += warps_per_grid){
+                for(int i=0 ;i < PAGE_SIZE/sizeof(data_type); i += sizeof(data_type)*32){
+                    int index = i * warp_id + lane_id;
+                    output[index] = accumulate(input[index]); //TODO do some accumulations!
+                }
+            }
+        }
+
+        template <typename data_type>
+        __global__
+        static void matAdd(int N, data_type *A, data_type *B, data_type *C){
+        
+            int index = threadIdx.x;
+            int stride = blockDim.x;
+            for(int i=index; i < N ; i += stride)
+                C[i] = A[i] + B[i];
+        }
+
+        template <typename data_type>
+       __global__
+       static void matDot(int N, data_type* A, data_type* B, data_type* C){
+       
+            int index = threadIdx.x;
+            int stride = blockDim.x;
+            for(int i=index; i < n ; i += stride)
+                C[i] = A[i] * B[i];
+       }
+
+        template <typenamd data_type>
+       __global__
+       static void matTranspose(int M, int N, data_type* A, data_type* B){
+           //current row and column for this thread to execute
+           int row = blockIdx.y*blockDim.y + threadIdx.y;
+           int col = blockIdx.x*blockDim.x + threadIdx.x;
+           if(row<M && col<N)
+               B[col*M + row] = A[row*N + col];
+       }
+
+  public:
+       static void MatMulAsync(float N, float *inA, float *inB, float *outC, cudaStream_t){
+
+           float *g_inA, *g_inB, *g_outC; //device memory 
+           cudaStream_t stream_0;
+           int size = N*N*sizeof(float);
+
+           cudaMalloc((void**)&g_inA, size); //malloc on device (writes device memory address) 
+           cudaMalloc((void**)&g_inB, size);
+           cudaMalloc((void**)&g_outC, size);
+           
+           //Asynchronously copies memory from Host memory..
+           cudaMemcpyAsync(g_inA, inA, size, cudaMemcpyHostToDevice, stream_0);
+           cudaMemcpyAsync(g_inB, inB, size, cudaMemcpyHostToDevice, stream_0);
+           
+           //allocate 256 threads per block 
+           //Uses multiple blocks to maximize multi-threading performance
+           //these values can be differentiated by gpu architecture
+           dim3 threadsPerBlock(16,16);
+           dim3 numBlocks(N/16,N/16);
+
+           //Start kernel
+           matMul<<<numBlocks ,threadsPerBlock ,0 ,stream_0>>>(g_inA, g_inB, outC, N);
+           cudaMemcpyAsync(outC, g_outC, size, cudaMemcpyDeviceToHosT, stream_0);
+           cudaStreamSynchronize(stream_0);//wait until processes on streak_0 is finished
+
+           //Free malloced resources on gpu
+           cudaFree(g_inA);
+           cudaFree(g_inB);
+           cudaFree(g_outC);
+       }
+
+       static void MatMulAsyncUnified(float N, float *inA, float *inB, float *outC){
+          
+       }
+
+}
