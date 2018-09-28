7c42ce7b5b52cbd9301ad32c11777b14aea8fa08
jwkim98 <jwkim98@kaist.ac.kr>
2018-09-28 16:55:03 +0900

    grid, block kernel call set

--- gpu_lib.cuh
+++ gpu_lib.cuh
@@ -2,15 +2,16 @@
 #define __GPU_LIB__
 
 #define PAGE_SIZE 1024*4
-#include <stdlib.h>
+#include <cstdlib>
 #include <functional>
+#include <iostream>
 
 namespace cuda_lib{
     class cudaMatrix{
 
       private:
             //for 2-dimensional Squre Matrix
-            void MatMul(float* A, float* B, float* C, int N);
+            void MatMul(int N, float* A, float* B, float* C);
 
             template<typename data_type>
             void MatMulUnified(int N ,data_type* A ,data_type* B, data_type* C);
@@ -32,7 +33,12 @@ namespace cuda_lib{
             void MatTranspose(int M, int N, data_type* A, data_type* B);
 
       public:
+            cudaMatrix(){
+                std::cout<<"cudaMatrix Initialized"<<std::endl;
+            }
+
             void MatMulAsync(float N, float *inA, float *inB, float *outC, cudaStream_t stream); 
+
     };
 
 
@@ -59,40 +65,55 @@ namespace cuda_lib{
 
 
 
-    void cudaMatrix::MatMul(float* A, float* B, float* C, int N){
-        matMul<<<1,1>>>(A,B,C,N);
+    void cudaMatrix::MatMul(int N, float* A, float* B, float* C){
+        dim3 block(32,32);
+        dim3 grid(N/32+1, N/32+1);
+        matMul<<<grid,block>>>(A,B,C,N);
     }
 
     template<typename data_type>
     void cudaMatrix::MatMulUnified(int N, data_type* A, data_type* B, data_type* C){
-        matMulUnified(N, A, B, C);
+        dim3 block(32,32);
+        dim3 grid(N/32+1, N/32+1);
+        matMulUnified<<<grid,block>>>(N, A, B, C);
     }
 
     template <typename data_type>
     void MatMulTwo(int M ,int MN ,int N ,data_type *input_A ,data_type* input_B ,data_type* output_C){
-        matMulTwo(M, MN, N, input_A, input_B, output_C);
+        dim3 block(32,32);
+        dim3 grid(M/32+1, N/32+1);
+        matMulTwo<<<grid,block>>>(M, MN, N, input_A, input_B, output_C);
     }
 
 
     //for Any 2-dimensional matrices
     template <typename data_type>
     void cudaMatrix::MatOp(int M ,int N ,data_type *input ,data_type* output ,std::function<data_type (data_type)> accumulate){
-        matop(M, N, input, output, accumulate);
+        dim3 block(32,32);
+        dim3 grid(M/32+1, N/32+1);
+        matop<<<grid,block>>>(M, N, input, output, accumulate);
     }
 
     template <typename data_type>
     void cudaMatrix::MatAdd(int N, data_type *A, data_type *B, data_type *C){
-        matAdd(N, A, B, C);
+        dim3* block = new dim3(32,32);
+        dim3 grid(N/32+1, N/32+1);
+        matAdd<<<grid,*block>>>(N, A, B, C);
+        delete block;
     }
     
     template <typename data_type>
     void cudaMatrix::MatDot(int N, data_type* A, data_type* B, data_type* C){
-        matDot(N, A, B, C);
+        dim3 block(32,32);
+        dim3 grid(N/32+1, N/32+1);
+        matDot<<<grid,block>>>(N, A, B, C);
     }
 
     template <typename data_type>
     void cudaMatrix::MatTranspose(int M, int N, data_type* A, data_type* B){
-        matTranspose(M, N, A, B);
+        dim3 block(32,32);
+        dim3 grid(M/32+1, N/32+1);
+        matTranspose<<<grid,block>>>(M, N, A, B);
     }
 }
 #endif
